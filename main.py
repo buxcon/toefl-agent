10a2589a01c54cf04bfa3397a9f418f85caebd73
buxcon <buxconz@gmail.com>
2021-02-02 18:13:46 +0800

    删除账号信息

--- main.py
+++ main.py
@@ -8,4 +8,3 @@ if __name__ == "__main__":
     username = input("用户名: ")
     password = input("密码: ")
     LoginUtils.mock_login(username, password)
-    # LoginUtils.mock_login("21328572", "Arey0umyxi0ngdi?")
